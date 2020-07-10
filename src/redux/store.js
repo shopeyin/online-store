d2920e6e401f038b3255b4cefde7b60154f4ef6b
oladimeji <tommyslipz@gmail.com>
2020-07-10 05:44:12 +0100

    commits

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 import thunk from "redux-thunk";
-
+import createSagaMiddleware from "redux-saga";
 const middlewares = [thunk];
 
 if (process.env.NODE_ENV === "development") {
